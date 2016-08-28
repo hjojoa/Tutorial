5da404d971be8b1032a03c169f7233b7110b6913
ususrio <hjojoa@unicauca.edu.co>
2016-08-28 11:33:12 -0500

    Primer Commit

---
+++ TutorialGithub/src/Logica/Hola_mundo.java
@@ -0,0 +1,18 @@
+package Logica;
+
+import javax.swing.JOptionPane;
+
+public class Hola_mundo 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Hola mundo Cruel");
+
+	}
+
+}
